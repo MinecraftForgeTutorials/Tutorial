3a27fc8e203c4181946fa5e1568465d6d487dec2
ViolentNinjaD <fiddlybilly@gmail.com>
2014-12-31 10:56:23 +0000

    First Part

---
+++ src/main/java/ninja/moddingtuts/ModdingTuts.java
@@ -0,0 +1,11 @@
+package ninja.moddingtuts;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = ModdingTuts.MOD_ID, name = ModdingTuts.MOD_NAME, version = ModdingTuts.MOD_VERSION)
+public class ModdingTuts 
+{
+    public static final String MOD_ID = "moddingtuts";
+    public static final String MOD_NAME = "Modding Tuts";
+    public static final String MOD_VERSION = "@VERSION@";
+}
